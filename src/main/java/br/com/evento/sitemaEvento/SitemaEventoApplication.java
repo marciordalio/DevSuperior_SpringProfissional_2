3d40e377964e7d53112392b1f7020d8dec805db0
marciordalio <marcioordalio1808@gmail.com>
2026-05-07 07:40:29 -0300

    firt commit

---
+++ src/main/java/br/com/evento/sitemaEvento/SitemaEventoApplication.java
@@ -0,0 +1,13 @@
+package br.com.evento.sitemaEvento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitemaEventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitemaEventoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/evento/sitemaEvento/SitemaEventoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.evento.sitemaEvento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitemaEventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
